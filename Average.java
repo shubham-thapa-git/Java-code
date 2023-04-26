7ff34152a58b8bc1a5840fb417bc3496f519ca91
shubham-thapa-git <shubham.thapa.1802@gmail.com>
2023-04-26 23:54:19 +0530

    new new

---
+++ Average.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Average{
+    
+    
+    public static int avgCalc(int a, int b, int c)
+    {
+        int avg=(a+b+c)/3;
+        return avg;
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter the first number");
+        a= sc.nextInt();
+        System.out.println("enter the second number");
+        b= sc.nextInt();
+        System.out.println("enter the third number");
+        c= sc.nextInt();
+        int avg = avgCalc(a,b,c);
+
+        System.out.println("The average of your inputs is:"+ avg);
+sc.close();
+    }
+}
---
+++ Multiplication.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static double multi(double a, double b){
+        double mul=(a*b);
+        return mul;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        double a= sc.nextDouble();
+        System.out.println("Enter the second number");
+        double b= sc.nextDouble();
+
+        double m= multi(a,b);
+        System.out.println("the multiple of the numbers are:"+ m);
+        sc.close();
+    }
+}
