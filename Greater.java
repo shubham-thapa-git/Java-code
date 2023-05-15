6fb28aa6c5e4cc76b819b84f0d67aa0d645e5e5a
shubham-thapa-git <shubham.thapa.1802@gmail.com>
2023-05-15 23:15:56 +0530

    more programs

---
+++ Greater.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Greater {
+
+    public static int compare(int a , int b)
+    {
+        int c;
+        if(a>b){
+            c=a;
+        }
+        else
+        c=b;
+        return c;
+    }
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first number");
+        a=sc.nextInt();
+        System.out.println("enter second number");
+        b=sc.nextInt();
+        int g=compare(a,b);
+        System.out.println("The greater number is "+g);
+        sc.close();
+    }
+}
